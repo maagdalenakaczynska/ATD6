eb8755cfa06cf523a5a4efb36b19ba376a46f706
maagdalenakaczynska <73854198+maagdalenakaczynska@users.noreply.github.com>
2021-06-29 16:22:34 +0200

    Add files via upload

---
+++ riak.py
@@ -0,0 +1,31 @@
+import string
+import riak
+
+myClient = riak.RiakClient(pb_port=8087, protocol='pbc')
+
+myBucket = myClient.bucket('ATD6')
+
+person = {"name" : "Adam",
+          "surname" : Krychowiak,
+          "age": "n/a"
+          "isMarried":"n/a"}
+
+newPerson = myBucket.new(person['name'], data = person)
+newPerson.store()
+print("Zapisano, key: " + myBucket.key + " | data = " + str(myBucket.data))
+
+fetched1 = myBucket.get(newPerson.key)
+print("Pobrano, key: " + fetched1.key + " | data = " + str(fetched1.data))
+
+fetched2 = fetched1.data
+fetched2["age"] = "25"
+fetched2["isMarried"] = "true"
+fetched1 = fetched2
+fetched1.store()
+
+fetched1 = myBucket.get(newPerson.key)
+print("Pobrano zaktualizowane, key: " + fetched1.key + " | data = " + str(fetched1.data))
+
+myBucket.delete(fetched1.key)
+print("Proba pobrania usunieteg: " + fetched1.key + " | data = " + str(myBucket.get(fetched1).data))
+
